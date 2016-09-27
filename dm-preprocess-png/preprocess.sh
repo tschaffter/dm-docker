5c96c0f570b3e643b5b2bacc3f2e69355b7a310b
tschaffter <thomas.schaffter@gmail.com>
2016-09-27 13:06:55 -0500

    Add generate_image_labels.py to PNG example

--- dm-preprocess-png/preprocess.sh
+++ dm-preprocess-png/preprocess.sh
@@ -5,8 +5,11 @@
 # - resize the images
 # - save to PNG format
 #
+# In addition, a labels file at the images levels is generated
+# using information from the exams metadata table (see generate_labels.py)
+#
 # Author: Thomas Schaffter (thomas.schaff...@gmail.com)
-# Last update: 2016-09-23
+# Last update: 2016-09-27
 #!/bin/bash
 
 TRAIN_DIRECTORY="/trainingData"
@@ -16,6 +19,5 @@ PP_IMAGES_DIRECTORY="$PP_DIRECTORY/images"
 mkdir -p $PP_IMAGES_DIRECTORY
 
 echo "Resizing and converting DICOM images to PNG format."
-find $TRAIN_DIRECTORY/ -name "*.dcm" | parallel --will-cite "convert {} -resize 500x500! $PP_IMAGES_DIRECTORY/{/.}.png"
-#find $TRAIN_DIRECTORY/ -name "*.dcm" | parallel --will-cite -m "mogrify -path $PP_DIRECTORY -resize 500x500! -format png {}" # slower than convert
+find $TRAIN_DIRECTORY/ -name "*.dcm" | parallel --will-cite "convert {} -resize 500x500! $PP_IMAGES_DIRECTORY/{/.}.png" # faster than mogrify
 echo "$(find $PP_IMAGES_DIRECTORY -name "*.png" | wc -l) PNG images have been successfully saved to $PP_IMAGES_DIRECTORY/."
